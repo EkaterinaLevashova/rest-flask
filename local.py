28c82316802bccfcbbf37c21cfbdeb36fb3bc7c7
Ekaterina Levashova <katrin.levashova@gmail.com>
2022-06-07 15:20:24 +0300

    added base RESTful api tech

--- local.py
+++ local.py
@@ -1,5 +1,8 @@
 import requests
 
-res = requests.get("http://127.0.0.1:3000/api/main")
+# res = requests.delete("http://127.0.0.1:3000/api/courses/1")
+# res = requests.post("http://127.0.0.1:3000/api/courses/3", {"name": "Golang", "videos": 5})
+# res = requests.post("http://127.0.0.1:3000/api/courses/4", {"name": "PHP", "videos": 15})
+res = requests.put("http://127.0.0.1:3000/api/courses/4", {"name": "PHPHPHPHp", "videos": 100})
 print(res.json())
 
--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api()
 
+courses = {
+    1: {"name": "Python", "videos": 15},
+    2: {"name": "Java", "videos": 13}
+}
+
+parser = reqparse.RequestParser()
+parser.add_argument("name", type=str)
+parser.add_argument("videos", type=int)
 
 class Main(Resource):
-    def get(self):
-        return {
-            "info": "some info",
-            "num": 42
-        }
+    def get(self, course_id):
+        if course_id == 0:
+            return courses
+        else:
+            return courses[course_id]
+
+    def delete(self, course_id):
+        del courses[course_id]
+        return courses
+
+    def post(self, course_id):
+        courses[course_id] = parser.parse_args()
+        return courses
+
+    def put(self, course_id):
+        courses[course_id] = parser.parse_args()
+        return courses
 
 
-api.add_resource(Main, "/api/main")
+api.add_resource(Main, "/api/courses/<int:course_id>")
 api.init_app(app)
 
 if __name__ == "__main__":
